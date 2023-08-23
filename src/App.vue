657a1642cce16fe3f98d77aef6b7e5d2f1be3eb0
Eva Siti Nurjanah <eva.tradersfamily@gmail.com>
2023-08-23 17:28:33 +0700

    first apps for task to do list

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-view />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  mounted() {
+    document.title = 'To Do List App';
+  },
+})
+</script>
